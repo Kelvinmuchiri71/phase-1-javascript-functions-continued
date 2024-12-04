e96737e9c2adc124809c5b5cfdff4a9ea18c2030
Kelvin Muchiri <muchirikelv71@gmail.com>
2024-12-04 14:49:52 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity= 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun('bathe my dog')
+
+const mondayWork = function(activity= 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork('work from home')
+
+function wrapAdjective (start = "*") {
+    return function(remark = "a hard worker") {
+        return `You are ${start}${remark}${start}!`;
+    }
+
+}
+
